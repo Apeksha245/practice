bfba25963dfb8d999c6281a6b17724f755bef5f9
Apeksha245 <apekshajadhav65@gmail.com>
2020-03-15 19:20:25 +0530

    Adding an other program

---
+++ src/com/tgt/even_odd.java
@@ -0,0 +1,15 @@
+package com.tgt;
+import java.util.*;
+public class even_odd
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter any number");
+        int n=sc.nextInt();
+        if(n%2==0)
+            System.out.println("Even number");
+        else
+            System.out.println("Odd number");
+    }
+}
